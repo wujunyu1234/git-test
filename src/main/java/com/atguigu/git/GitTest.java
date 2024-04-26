9f4d26c95694db1cbdbeae392884cad6b277991e
wujunyu <1846855143@qq.com>
2024-04-26 10:01:32 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello github!");
         System.out.println("hot-fix test!");
         System.out.println("push github!");
+        System.out.println("sss");
     }
 }
